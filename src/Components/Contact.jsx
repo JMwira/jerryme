cee3e02fd44c12c98c4890bf940e892cac150481
JMwira <jaymwira@gmail.com>
2023-05-22 11:47:29 +0100

    contact01

--- src/Components/Contact.jsx
+++ src/Components/Contact.jsx
@@ -5,39 +5,38 @@ import {FaTwitter, FaLinkedinIn} from "react-icons/fa"
 
 const Contact = () => {
   return (
-    <div className='w-[85%] flex flex-col mx-auto justify-center items-center' >
+    <div className='w-[90%] sm:w-[85%] flex flex-col mx-auto justify-center items-center' >
       <span className='text-4xl font-light text-center' >Hey there<br/>I would like to hear from you</span>
       <div className='text-gray-400 w-full flex flex-col gap-x-2 pt-8 items-start' >
         
-        <div className='flex-1 mx-auto w-[70%] text-center' >
+        <div className='flex-1 mx-auto w-[90%] sm:w-[70%] text-center' >
             <span className='font-light text-sm' >
               Do you have a project in mind? or just an enquiry?
               <br/>
               Contact me by filling this form or straight linking to my social media
             </span>
             <div className='flex flex-col gap-y-2 mt-5 text-sm font-light' >
-                <input className='text-center py-2 bg-transparent border-b-[1px] px-2' placeholder='email' />
-                <textarea className='text-center py-2 bg-transparent border-b-[1px] px-2 min-h-[100px] max-h-[100px]' placeholder='enter your message here' />
+                <input className='text-center py-2 bg-transparent border-b-2 px-2' placeholder='email' />
+                <textarea className='text-center py-2 bg-transparent border-b-2 px-2 min-h-[100px] max-h-[100px]' placeholder='enter your message here' />
                 <button className='bg-invert p-2 px-10 text-primary rounded-full w-[200px] mx-auto mt-5' >Send</button>
             </div>
         </div>
-        <div className='flex mx-auto pb-6 flex-row gap-x-8 mt-5 font-light' >
-            
-            <div className='flex items-start gap-x-2'>
+        <div className='gap-y-[100px] sm:gap-y-0 sm:flex mx-auto pb-6 flex-row gap-x-8 font-light' >            
+            <div className='flex items-start gap-x-2 mt-5'>
                 <div className='bg-gray-200 rounded-full text-primary p-2'><MdLocationPin className='text-lg'/></div>
                 <div className='flex flex-col'>
                     <span>Goma/DRC</span>
                     <span>Av 30 juin</span>
                 </div>
             </div>  
-            <div className='flex items-start gap-x-2'>
+            <div className='flex items-start gap-x-2 mt-5'>
                 <div className='bg-gray-200 rounded-full text-primary p-2'><MdEmail className='text-lg'/></div>
                 <div className='flex flex-col'>
                     <span className=''>jeymwira@gmail.com</span>
                     <span className=''>jaymwira@gmail.com</span>
                 </div>
             </div>
-            <div className='flex items-start gap-x-2'>
+            <div className='flex items-start gap-x-2 mt-5'>
                 <div className='bg-gray-200 rounded-full text-primary p-2'><MdPhone className='text-lg'/></div>
                 <div className='flex flex-col'>
                     <span>+243 973 281 762</span>
