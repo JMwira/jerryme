1a26b789a5803f1f245e89990aa73cff7400c505
JMwira <jaymwira@gmail.com>
2023-05-22 11:49:45 +0100

    Data02

--- src/data.js
+++ src/data.js
@@ -1,131 +1,33 @@
 // import a from "./assets/"
 export const data = [
     {
-        id:"",
-        project_name:"",
-        project_img:[],
-        project_description:"",
-        project_tag:[]
-
-    },
-    {
-        id:"",
-        project_name:"",
-        project_img:[],
-        project_description:"",
-        project_tag:[]
-
-    },
-    {
-        id:"",
-        project_name:"",
-        project_img:[],
-        project_description:"",
-        project_tag:[]
-
-    },
-    {
-        id:"",
-        project_name:"",
-        project_img:[],
-        project_description:"",
-        project_tag:[]
-
-    },
-    {
         id:1,
-        project_name:"",
+        project_name:"Pyramid",
         project_img:[],
-        project_description:"",
-        project_tag:[]
+        project_description:"En tant que développeur chez Pyramid RDC, j'ai concentré mon attention sur la fourniture d'un logiciel qui leur permet de numériser et d'améliorer leurs services en matière de ramassage des ordures.",
+        project_tag:['react redux','react native', 'firebase', 'mobile dev'],
+        category:'mobile',
+        url:''
 
     },
     {
         id:2,
-        project_name:"",
+        project_name:"Bienestar wellness",
         project_img:[],
-        project_description:"",
-        project_tag:[]
+        project_description:"Site publicitaire pour assurer la presence mediatique de l'entreprise bienestar wellness club",
+        project_tag:['react js', 'tailwind css'],
+        category:'web',
+        url:'https://bienes.pages.dev'
 
     },
     {
         id:3,
-        project_name:"",
+        project_name:"portfolio template",
         project_img:[],
-        project_description:"",
-        project_tag:[]
+        project_description:"un simple template portfolio en apprennant certains concepts basiques sur react js et tailwind css",
+        project_tag:[],
+        category:'web',
+        url:'https://portfolio-5pp.pages.dev'
 
-    },
-    {
-        id:3,
-        project_name:"",
-        project_img:[],
-        project_description:"",
-        project_tag:[]
-
-    },
-    {
-        id:3,
-        project_name:"",
-        project_img:[],
-        project_description:"",
-        project_tag:[]
-
-    },
-    {
-        id:3,
-        project_name:"",
-        project_img:[],
-        project_description:"",
-        project_tag:[]
-
-    },
-    {
-        id:3,
-        project_name:"",
-        project_img:[],
-        project_description:"",
-        project_tag:[]
-
-    },
-    {
-        id:3,
-        project_name:"",
-        project_img:[],
-        project_description:"",
-        project_tag:[]
-
-    },
-    {
-        id:3,
-        project_name:"",
-        project_img:[],
-        project_description:"",
-        project_tag:[]
-
-    },
-    {
-        id:3,
-        project_name:"",
-        project_img:[],
-        project_description:"",
-        project_tag:[]
-
-    },
-    {
-        id:3,
-        project_name:"",
-        project_img:[],
-        project_description:"",
-        project_tag:[]
-
-    },
-    {
-        id:3,
-        project_name:"",
-        project_img:[],
-        project_description:"",
-        project_tag:[]
-
-    },
+    }
 ]
