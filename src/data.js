0823f0a014b0d309855f2324504e80fc0b0f83ba
JMwira <jaymwira@gmail.com>
2023-05-25 22:17:42 +0100

    index01

--- src/data.js
+++ src/data.js
@@ -1,9 +1,18 @@
-// import a from "./assets/"
+import a from "./assets/py/pyramid-1.jpg"
+import b from "./assets/py/pyramid-2.jpg"
+import c from "./assets/py/pyramid-3.jpg"
+import d from "./assets/py/pyramid-4.jpg"
+import e from "./assets/py/pyramid-5.jpg"
+import f from "./assets/py/pyramid-6.jpg"
+import g from "./assets/py/pyramid-7.jpg"
+import h from "./assets/py/pyramid-8.jpg"
+import i from "./assets/py/pyramid-9.jpg"
+import j from "./assets/py/pyramid-10.jpg"
 export const data = [
     {
         id:1,
         project_name:"Pyramid",
-        project_img:[],
+        project_img:[a,b,c,d,e,f,g,h,i,j],
         project_description:"En tant que développeur chez Pyramid RDC, j'ai concentré mon attention sur la fourniture d'un logiciel qui leur permet de numériser et d'améliorer leurs services en matière de ramassage des ordures.",
         project_tag:['react redux','react native', 'firebase', 'mobile dev'],
         category:'mobile',
@@ -25,9 +34,29 @@ export const data = [
         project_name:"portfolio template",
         project_img:[],
         project_description:"un simple template portfolio en apprennant certains concepts basiques sur react js et tailwind css",
-        project_tag:[],
+        project_tag:['react js', 'tailwind css'],
         category:'web',
         url:'https://portfolio-5pp.pages.dev'
+
+    },
+    {
+        id:4,
+        project_name:"ASAF university",
+        project_img:[],
+        project_description:"un simple template portfolio en apprennant certains concepts basiques sur react js et tailwind css",
+        project_tag:['react redux','react js', 'web dev'],
+        category:'web',
+        url:'https://www.asafuniversity.co/'
+
+    },
+    {
+        id:5,
+        project_name:"GBB africa",
+        project_img:[],
+        project_description:"un simple template portfolio en apprennant certains concepts basiques sur react js et tailwind css",
+        project_tag:['react redux','react js', 'web dev'],
+        category:'web',
+        url:'https://gbb-africa.org'
 
     }
 ]
