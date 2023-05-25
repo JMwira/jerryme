888660e146a40d6390d09d236e95b664916ba2c5
JMwira <jaymwira@gmail.com>
2023-05-25 22:17:01 +0100

    skills02

--- src/Components/Skills.jsx
+++ src/Components/Skills.jsx
@@ -21,23 +21,28 @@ const Skills = () => {
                 {/* <Line percent={75} className='rounded-full mt-2' strokeWidth={2} strokeColor="#219ebc" trailWidth={5} /> */}
             </div>
             <div className='text-invert flex flex-col border-b-[1px] border-invert mt-2 pb-5' >
+                <span>Tailwind css</span>
+                <span className='text-sm font-light text-gray-400' >Component styling</span>
+                {/* <Line percent={75} className='rounded-full mt-2' strokeWidth={2} strokeColor="#219ebc" trailWidth={5} /> */}
+            </div>
+            <div className='text-invert flex flex-col border-b-[1px] border-invert mt-2 pb-5' >
                 <span>Node js</span>
                 <span className='text-sm font-light text-gray-400' >APIs development</span>
                 {/* <Line percent={75} className='rounded-full mt-2' strokeWidth={2} strokeColor="#219ebc" trailWidth={5} /> */}
             </div>
             <div className='text-invert flex flex-col border-b-[1px] border-invert mt-2 pb-5' >
                 <span>React redux</span>
-                <span className='text-sm font-light text-gray-400' >front end web development</span>
+                <span className='text-sm font-light text-gray-400' >State management</span>
                 {/* <Line percent={70} className='rounded-full mt-2' strokeWidth={2} strokeColor="#219ebc" trailWidth={5} /> */}
             </div>
             <div className='text-invert flex flex-col border-b-[1px] border-invert mt-2 pb-5' >
                 <span>Firebase</span>
-                <span className='text-sm font-light text-gray-400' >database management</span>
+                <span className='text-sm font-light text-gray-400' >NoSql database management</span>
                 {/* <Line percent={75} className='rounded-full mt-2' strokeWidth={2} strokeColor="#219ebc" trailWidth={5} /> */}
             </div>
             <div className='text-invert flex flex-col border-b-[1px] border-invert mt-2 pb-5' >
                 <span>mongoDb</span>
-                <span className='text-sm font-light text-gray-400' >database management</span>
+                <span className='text-sm font-light text-gray-400' >NoSql database management</span>
                 {/* <Line percent={80} className='rounded-full mt-2' strokeWidth={2} strokeColor="#219ebc" trailWidth={5} /> */}
             </div>
             <div className='text-invert flex flex-col border-b-[1px] border-invert mt-2 pb-5' >
@@ -47,12 +52,12 @@ const Skills = () => {
             </div>
             <div className='text-invert flex flex-col border-b-[1px] border-invert mt-2 pb-5' >
                 <span>Git version control</span>
-                <span className='text-sm font-light text-gray-400' >front end web development</span>
+                <span className='text-sm font-light text-gray-400' >version control</span>
                 {/* <Line percent={75} className='rounded-full mt-2' strokeWidth={2} strokeColor="#219ebc" trailWidth={5} /> */}
             </div>
             <div className='text-invert flex flex-col border-b-[1px] border-invert mt-2 pb-5' >
                 <span>Agile development methodology</span>
-                <span className='text-sm font-light text-gray-400' >front end web development</span>
+                <span className='text-sm font-light text-gray-400' >Development methodology</span>
                 {/* <Line percent={70} className='rounded-full mt-2' strokeWidth={2} strokeColor="#219ebc" trailWidth={5} /> */}
             </div>
         </div>
