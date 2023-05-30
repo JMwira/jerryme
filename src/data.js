1a8e1f528c369065e1390c9be819d831af040871
JMwira <jaymwira@gmail.com>
2023-05-30 08:28:24 +0100

    index02

--- src/data.js
+++ src/data.js
@@ -13,7 +13,7 @@ export const data = [
         id:1,
         project_name:"Pyramid",
         project_img:[a,b,c,d,e,f,g,h,i,j],
-        project_description:"En tant que développeur chez Pyramid RDC, j'ai concentré mon attention sur la fourniture d'un logiciel qui leur permet de numériser et d'améliorer leurs services en matière de ramassage des ordures.",
+        project_description:"As a developer at Pyramid RDC, I focused my attention on providing software that allows them to digitize and improve their garbage collection services.",
         project_tag:['react redux','react native', 'firebase', 'mobile dev'],
         category:'mobile',
         url:''
@@ -23,7 +23,7 @@ export const data = [
         id:2,
         project_name:"Bienestar wellness",
         project_img:[],
-        project_description:"Site publicitaire pour assurer la presence mediatique de l'entreprise bienestar wellness club",
+        project_description:"Advertising site to ensure the media presence of the company bienestar wellness club",
         project_tag:['react js', 'tailwind css'],
         category:'web',
         url:'https://bienes.pages.dev'
@@ -33,7 +33,7 @@ export const data = [
         id:3,
         project_name:"portfolio template",
         project_img:[],
-        project_description:"un simple template portfolio en apprennant certains concepts basiques sur react js et tailwind css",
+        project_description:"a simple portfolio completed as i was learning basic concepts on react js and tailwind css",
         project_tag:['react js', 'tailwind css'],
         category:'web',
         url:'https://portfolio-5pp.pages.dev'
@@ -43,7 +43,7 @@ export const data = [
         id:4,
         project_name:"ASAF university",
         project_img:[],
-        project_description:"un simple template portfolio en apprennant certains concepts basiques sur react js et tailwind css",
+        project_description:"university website for the university Other Sounds from Africa",
         project_tag:['react redux','react js', 'web dev'],
         category:'web',
         url:'https://www.asafuniversity.co/'
@@ -53,7 +53,7 @@ export const data = [
         id:5,
         project_name:"GBB africa",
         project_img:[],
-        project_description:"un simple template portfolio en apprennant certains concepts basiques sur react js et tailwind css",
+        project_description:"NGO website",
         project_tag:['react redux','react js', 'web dev'],
         category:'web',
         url:'https://gbb-africa.org'
